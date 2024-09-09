54534741788816dde76916cd38305a07c5b500bf
Yanni <yannitrim@gmail.con>
2024-09-08 21:57:25 -0400

    New Github Action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    images: {
+      loader: "akamai",
+      path: "",
+    },
+    assetPrefix: "./",
+  };
+  
+  export default nextConfig;
