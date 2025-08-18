f1c71959ee3b77036135658f54ad869d71a953da
Yanni <yannitrim@gmail.con>
2025-08-18 16:30:25 -0400

    Recent info undate

--- app/page.js
+++ app/page.js
@@ -6,41 +6,6 @@ export default function Home() {
 
   const [menuOpen, setMenuOpen] = useState(false);
 
-//   const [messageInput, setMessageInput] = useState("");
-
-//   const [messages, setMessages] = useState([
-//     {
-//       "role": "system",
-//       "content": "Hello, how can I help you today?"
-//     }
-//   ]);
-
-//   const submitForm = async (e) => {
-//     try {
-//       e.preventDefault();
-//       let newMessages = [...messages, {role: "user", content: messageInput}];
-//       setMessages(newMessages);
-//       setMessageInput("");
-//       const res = await fetch(
-//         ("./api/route"), 
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json"
-//           },
-//           body: JSON.stringify({messages: newMessages}),
-//         }
-//       )
-//       const data = await res.json();
-//       console.log(data.message)
-//       setMessages([...newMessages, {role: "system", content: data.message}]);
-//     } catch (error) {
-//         console.error("page.js error:", error);
-//         console.error("Error fetching chat completions:", error);
-//         setMessages([...messages, {role: "system", content: "Failed to fetch response from OpenAI."}]);
-//     }
-//   };
-
   const toggleMobileMenu = () => {
     setMenuOpen(!menuOpen);
   }
@@ -75,16 +40,15 @@ export default function Home() {
                           Yanni Trimikliniotis
                       </h1>
                       <p>
-                          A junior at New York University double majoring in Computer
-                          Science and History with Honors. I work as both a Computer Science Instructor teaching students 
-                          about HTML, CSS, JavaScript, Java, and Python and as a Supplemental Instructor for Calculus at NYU.
-                          <span>I'm interested in Software Engineering, Game Design, and Data Analysis.
-                              At the same time, I love history, economics, and writing which explains my choice in a second major.
+                          A senior at New York University double majoring in Computer
+                          Science and History with Honors. I work as a Supplemental Instructor at NYU where I tutor Computer Science and Calculus to other students.
+                          <span> I'm interested in Software Engineering, Game Design, and Data Analysis.
+                              At the same time, I love history, philosophy, and writing which explains my choice in a second major.
                           </span>
                       </p>
                       <div className="call-to-action">
                           <a href="./assets/YanniTrimikliniotisResume.pdf" className="button black">View Resume</a>
-                          <a href="mailto:yt2575@nyu.edu" className="button white">Contact Me</a>
+                          <a href="mailto:yannitrim@gmail.com" className="button white">Contact Me</a>
                       </div>
                       <div className="social-links">
                           <a href="https://github.com/YanniTrim">
@@ -97,7 +61,7 @@ export default function Home() {
                   </div>
               </div>
               <div className="hero-yellow">
-                  <img src="assets/headshot.jpg" alt="Yanni Trimikliniotis" width="100%"/>
+                  <img src="assets/headshot1.jpg" alt="Yanni Trimikliniotis" width="100%"/>
               </div>
           </section>
           <section className="logos container">
@@ -112,7 +76,8 @@ export default function Home() {
                       <img src="assets/python.png" alt="Python" width="128"/>
                       <img src="assets/nextjs.png" alt="Next.js" width="128" />
                       <img src="assets/azure.png" alt="Azure" width="128" />
-                      <img src="assets/vscode.png" alt="VS Code" width="128"/>  
+                      <img src="assets/vscode.png" alt="VS Code" width="128"/> 
+                    <img src="assets/jira.png" alt="Jira" width="128"/> 
                       <img src="assets/html.png" alt="HTML" width="128"/>
                       <img src="assets/css.png" alt="CSS" width="128"/>
                       <img src="assets/sass.png" alt="SaSS" width="128" />
@@ -123,6 +88,7 @@ export default function Home() {
                       <img src="assets/nextjs.png" alt="Next.js" width="128" />
                       <img src="assets/azure.png" alt="Azure" width="128" />
                       <img src="assets/vscode.png" alt="VS Code" width="128"/>
+                      <img src="assets/jira.png" alt="Jira" width="128"/>
                       <img src="assets/html.png" alt="HTML" width="128"/>
                       <img src="assets/css.png" alt="CSS" width="128"/>
                       <img src="assets/sass.png" alt="SaSS" width="128" />
@@ -133,6 +99,7 @@ export default function Home() {
                       <img src="assets/nextjs.png" alt="Next.js" width="128" />
                       <img src="assets/azure.png" alt="Azure" width="128" />
                       <img src="assets/vscode.png" alt="VS Code" width="128"/> 
+                      <img src="assets/jira.png" alt="Jira" width="128"/>
                   </div>
               </div>
           </section>
@@ -145,10 +112,11 @@ export default function Home() {
                   <div className="left-column">
                       <h3>Frontend</h3>
                       <ul>
-                          <li>HTML</li>
-                          <li>CSS</li>
-                          <li>SaSS</li>
-                          <li>JavaScript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>SaSS</li>
+                            <li>React</li>
+                            <li>JavaScript</li>
                       </ul>
                       <h3>Backend</h3>
                       <ul>
@@ -164,26 +132,30 @@ export default function Home() {
                         <li>Figma</li>
                         <li>Git</li>
                         <li>MongoDB</li>
+                        <li>Jira</li>
                       </ul>
                   </div>
                   <div className="middle-column">
                       <h3>Relevant Coursework</h3>
                       <ul>
+                        {/* <li>Computer Grahics (Graduate Level)</li>
+                        <li>Algorithmic Problem Solving</li> */}
                           <li>Basic Algorithms</li>
                           <li>Applied Internet Technology</li>
                           <li>Computer Systems Organization</li>
                           <li>Data Structures</li>
+                          <li>Discrete Math</li>
                       </ul>
                   </div>
                   <div className="right-column">
                       <h3>A bit about me</h3>
-                      <p>Hi I'm Yanni, a junior at New York University double majoring in Computer
-                          Science and History with Honors. I work as both a Computer Science Instructor teaching students 
-                          about HTML, CSS, JavaScript, Java, and Python and as a Supplemental Instructor for Calculus at NYU.
+                      <p>Hi I'm Yanni, a senior at New York University double majoring in Computer
+                          Science and History with Honors. I work as a Supplemental Instructor for Calculus at NYU. 
                           I'm interested in Software Engineering, Game Design, and Data Analysis.
-                          At the same time, I love history, economics, and writing which explains my choice in a second major.</p>
-                      <p>I'm currently working on a project that uses the Spotify and OpenAI APIs to create custom images to act as playlist covers, based on the songs 
-                          in the playlist.</p>
+                          At the same time, I love history, philosophy, and writing which explains my choice in a second major.</p>
+                      <p>I have worked on fullstack applications, mainly utilizing the MERN stack as seen in my Workout Tracker which I am continuing to add to overtime.
+                        I am also working on a History thesis for my honors program where I am investigating the leveraging of nationalism and nation-building in authoritarian regimes and the subsequent tension that is found in imperial ambitions.
+                      </p>
                   </div>
               </div>
           </section>
@@ -193,6 +165,21 @@ export default function Home() {
                   Work Experience
               </h2>
               <div className="jobs">
+                    <article>
+                        <figure>
+                            <div>
+                                <img src="assets/barclays.jpg" alt="Barclays" width="100%"/>
+                                <figcaption>Barclays</figcaption>
+                            </div>
+                        </figure>
+                        <h3>Software Engineer Intern</h3>
+                        <div>June 2025-August 2025</div>
+                        <ul>
+                            <li>Automated organizational processes in JavaScript for the Data Governance documentation by leveraging content analysis through natural language processing, Confluence API navigation, rate limit handling, and fuzzy-string matching</li>
+                            <li>Identified faults in data flows and records storage, which could cause financial and legal risks</li>
+                            <li>Designed record retention solutions closing legal risks and, upon implementation, reducing SMS record costs by 50%</li>
+                        </ul>
+                    </article>
                   <article>
                       <figure>
                           <div>
@@ -249,19 +236,32 @@ export default function Home() {
                   Projects
               </h2>
               <div className="bento-grid">
+                <div className="bento-item">
+                    <h3>RPG Workout Tracker</h3>
+                    <h4>October 2024 - December 2024</h4>
+                    <p className="languages">React, NodeJS, Express.js, MongoDB</p>
+                    <p className="desc">A fullstack web application that allows users to track their workouts and progress in a role-playing game style.
+                        Users can create an account, log in, and track their workouts, which are then converted into experience points and levels. There are progression systems in place so users can set goals and earn rewards as they level up.
+                    </p>
+                    <div className="project-buttons">
+                         <a href="https://github.com/YanniTrim/WorkoutTracker/tree/main">
+                              <img src="assets/github.png" alt="GitHub" width="48"/>
+                          </a>
+                    </div> 
+                </div>
                   <div className="bento-item">
                       <h3>Spotify Enhancer Project</h3>
-                      <h4>Currently Working On</h4>
+                      <h4>August 2024 - October 2024</h4>
                       <p className="languages">React, NodeJS</p>
                       <p className="desc">Web app where users can import a Spotify playlist and get a playlist cover image made specifcally for that playlist.
                           The cover is generated by DALLE in the OpenAI API using the genres, tags, and moods associated with each song in
                           the playlist. This allows the generated cover to be custom-made for that specific playlist. 
                       </p>
                       <div className="project-buttons">
-                          <a href="#">
+                         <a href="#">
                               <img src="assets/github.png" alt="GitHub" width="48"/>
                           </a>
-                      </div>
+                      </div> 
                   </div>
                   <div className="bento-item">
                       <h3>Matrix Calculator</h3>
@@ -301,42 +301,6 @@ export default function Home() {
                   </div>
               </div>
           </section>
-          {/* <section className="chatbot container">
-              <h2>
-                  <small>Talk to me</small>
-                  Chatbot
-              </h2>
-              <div className="chatbot-blue">
-                  <div className="chat-info">
-                      <h3>Azure AI Chatbot</h3>
-                      <p>I've put together a chatbot which knows all my skills,
-                          work experience and has a copy of my Resume! You can use it to ask
-                          questions about me to get a better idea of who I am and what I've done.
-                      </p>
-                      <p>You can also download the resume here if you want to take a look at it.
-                          I'm currently looking for internships and new opportunites so please reach out
-                          if there's anything you think I'd be a good fit for!
-                      </p>
-                      <a href="./assets/YanniTrimikliniotisResume.pdf" className="black button">Download Resume</a>
-                  </div>
-                  <div className="chat-box">
-                      <div className="scroll-area">
-                          <ul id="chat-log">
-                            {messages.map((message, index) => (
-                              <li key={index} className={`${message.role}`}>
-                                <span className={`avatar ${message.role}`}>{message.role === "user" ? "User" : "AI"}</span>
-                                <div className="message">{message.content}</div>
-                              </li>
-                            ))}
-                          </ul>
-                      </div>
-                      <form onSubmit={submitForm} className="chat-message">
-                          <input type="text" placeholder="Hey Yanni, what skills are you best at?" value={messageInput} onChange={e=> setMessageInput(e.target.value)}/>
-                          <button className="button black">Send</button>
-                      </form>
-                  </div>
-              </div>
-          </section> */}
       </main>
     </>
   );
